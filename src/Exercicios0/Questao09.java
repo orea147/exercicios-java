5427cebb536bde90bb32d571b3afa1388c7154c6
Matheus Rennan <matheus.teotonio@brisabr.com.br>
2024-08-14 12:02:01 -0300

    feat: Adicionar questoes 8 e 9

---
+++ src/Exercicios0/Questao09.java
@@ -0,0 +1,10 @@
+package Exercicios0;
+
+public class Questao09 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            int quadrado = i * i;
+            System.out.printf("O quadrado de %d é %d.%n", i, quadrado);
+        }
+    }
+}
