9bc563726cdc65a5dcb80c15ad88cdc93c82915b
Matheus Rennan Freire Veras Teotonio <Matheus.Teotonio@brisabr.com.br>
2024-08-14 15:25:28 -0300

    feat: Adicionar questoes 10 e 11

---
+++ src/Exercicios0/Questao10.java
@@ -0,0 +1,11 @@
+package Exercicios0;
+
+public class Questao10 {
+    public static void main(String[] args) {
+        int primeira = 6;
+        int segunda = 4;
+        int terceira = primeira / segunda;
+
+        System.out.println("O valor da terceira variável é: " + terceira);
+    }
+}
---
+++ src/Exercicios0/Questao11.java
@@ -0,0 +1,29 @@
+package Exercicios0;
+
+import java.util.Scanner;
+
+public class Questao11 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o código do produto: ");
+        String codigo = scanner.nextLine();
+
+        String produto;
+
+        if (codigo.equals("001")) {
+            produto = "Parafuso";
+        } else if (codigo.equals("002")) {
+            produto = "Porca";
+        } else if (codigo.equals("003")) {
+            produto = "Prego";
+        } else {
+            produto = "Diversos";
+        }
+
+        System.out.println("O produto correspondente ao código " + codigo + " é: " + produto);
+
+        scanner.close();
+    }
+}
+
